9cbcb80cf15b3018446f4460d0e737516670f49a
Shripad Kale <50066426+kaleshripad11@users.noreply.github.com>
2020-07-26 20:35:14 +0530

    Update 03_ShowDatabases.py

--- DatabaseConn/03_ShowDatabases.py
+++ DatabaseConn/03_ShowDatabases.py
@@ -1,6 +1,6 @@
 from mysql import connector as c
 
-con = c.connect(host='localhost',user='shree',password='shree@123')
+con = c.connect(host='localhost',user='myuser',password='user@123')
 cur = con.cursor()
 cur.execute("show databases;")
 for i in cur:
